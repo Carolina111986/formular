d05e3984d79755f32e53e6123eaed472d0c4b0fe
Wolfgang Hennerbichler <wogri@wogri.com>
2022-03-27 21:55:05 +0200

    fix error message

--- rundumsbauchgefuehl.at/frontend/src/app/error-dialog/error-dialog.component.ts
+++ rundumsbauchgefuehl.at/frontend/src/app/error-dialog/error-dialog.component.ts
@@ -21,7 +21,7 @@ export class ErrorDialogComponent implements OnInit {
 
   errorText(): string {
     if ( 500 <= this.data.error.status && this.data.error.status < 600) {
-      return "Server Fehler. Fehlercode " + this.data.error.status + ". Bitte später nochmals probieren.";
+      return "Server Fehler. Fehlercode " + this.data.error.status + ". Bitte probieren Sie es später nochmals oder bestellen Sie direkt per E-Mail bei office@rundumsbauchgefuehl.at.";
     } else if (403 === this.data.error.status) {
       return "Anmeldefehler. Bitte aus- und wieder einloggen.";
     } else if (400 <= this.data.error.status && this.data.error.status < 500) {
